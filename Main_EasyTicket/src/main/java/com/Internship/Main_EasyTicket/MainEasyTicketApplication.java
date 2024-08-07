554613590d4b2a8791b3f2fc939e6b751e8d31ef
mohamed <ettalbimohamed.work@gmail.com>
2024-08-07 09:36:03 +0100

    first commit

---
+++ Main_EasyTicket/src/main/java/com/Internship/Main_EasyTicket/MainEasyTicketApplication.java
@@ -0,0 +1,13 @@
+package com.Internship.Main_EasyTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainEasyTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainEasyTicketApplication.class, args);
+	}
+
+}
---
+++ Main_EasyTicket/src/test/java/com/Internship/Main_EasyTicket/MainEasyTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Internship.Main_EasyTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainEasyTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
